4026f713956bd290da668221d261e3c288aff0d5
fry <a1935af@aiit.ac.jp>
2019-04-21 08:08:14 +0000

    test

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+    # app/views/りーそす名/アクション名.html.erb
+    # app/views/static_pages/home.html.erb
+  end
+
+  def help
+  end
+
+  def about
+    # 'app/views/static_pages/about.html.erb'
+  end
+
+end

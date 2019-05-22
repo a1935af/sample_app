1f87a1a2d0df213f78b14efa0764ed1468efae51
fry <a1935af@aiit.ac.jp>
2019-05-22 15:42:46 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy  
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   #GET /users/:id
   def show
    @user = User.find(params[:id])
@@ -6,9 +14,11 @@ class UsersController < ApplicationController
    # => app/views/users/show.html.erb
   end
   
+  # GET /users/new
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,10 +31,61 @@ class UsersController < ApplicationController
     end
   end
   
-  def user_params
-    params.require(:user).permit(
-      :name, :email, :password,
-      :password_confirmation)
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # => app/views/users/edit.html.erb
   end
   
+  # PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Success
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      #Failure
+      # @user.errs.full_messagers()
+      render 'edit'
+    end 
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  #以下の宣言は、privateとなる
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :name, :email, :password,
+        :password_confirmation)
+    end
+  
+    # ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      # GET /users/:id/edit
+      # PACTH /users/:id
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
